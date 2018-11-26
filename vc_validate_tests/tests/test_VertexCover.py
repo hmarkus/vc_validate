2833050d993f76a51ab4f3fee1081e4e16be47aa
Markus Hecher <mhecher@gmail.com>
2018-11-26 13:17:08 +0100

    easy testcase added

--- vc_validate_tests/tests/test_VertexCover.py
+++ vc_validate_tests/tests/test_VertexCover.py
@@ -3,6 +3,19 @@ from __future__ import absolute_import
 
 import logging
 import unittest
+import os
+import inspect
+import sys
+
+src_path = os.path.abspath(os.path.realpath(inspect.getfile(inspect.currentframe())))
+lib_path = os.path.realpath(os.path.join(src_path, '../../../lib/htd_validate'))
+#print src_path
+if lib_path not in sys.path:
+    sys.path.insert(0, lib_path)
+
+import vc_validate
+import htd_validate
+
 
 class TestVertexCover(unittest.TestCase):
 
@@ -12,5 +25,7 @@ class TestVertexCover(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def testTest(self):
-        self.assertTrue(True)
+    def testSimple(self):
+        VC = vc_validate.vertexcover.VertexCover.from_file(os.path.realpath(os.path.join(src_path, "../v")), strict=True)
+        HG = htd_validate.utils.Graph.from_file(os.path.realpath(os.path.join(src_path, "../g")))
+        self.assertTrue(VC.validate(HG))
